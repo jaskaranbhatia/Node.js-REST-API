48cdbbdf3c34a15505d3e0aa2fefac03f42835d2
jaskaranbhatia <jaskaransingh.singh77@gmail.com>
2021-06-17 09:37:53 +0530

    MongoDB Configured

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const dotenv = require('dotenv')
+const connectDB = require('./config/db')
 
 dotenv.config({path : './config/config.env'});
+
+//Connecting to MongoDB
+connectDB();
 
 //Route Files
 const bootcamps = require('./routes/bootcamps');
@@ -13,6 +17,12 @@ app.use('/api/bootcamps', bootcamps);
 
 const PORT = process.env.PORT
 
-app.listen(PORT, ()=>{
+const server = app.listen(PORT, ()=>{
     console.log(`Server running on ${PORT} PORT`);
+});
+
+// Handling unhandled exceptions
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error : ${err.message}`);
+    server.close(() => process.exit(1));
 });
