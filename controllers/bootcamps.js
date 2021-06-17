a526187281f66d5ea6e967bba083a95a2129c027
jaskaranbhatia <jaskaransingh.singh77@gmail.com>
2021-06-17 14:35:42 +0530

    CRUD for Bootcamps Completed

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -8,6 +8,7 @@ exports.getBootcamps = async (req, res, next) => {
         const bootcamps = await Bootcamp.find();
         res.status(200).json({
             success: true,
+            count: bootcamps.length,
             data: bootcamps
         });
     }
@@ -57,5 +58,41 @@ exports.createBootcamp = async (req, res, next) => {
         })
     }
 }
+
+// @desc -> update single bootcamp
+// @route -> PUT /api/bootcamps/:id
+// @access -> Public
+exports.updateBootcamp = async (req, res, next) => {
+    try{
+        const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body,{
+            new: true,
+            runValidators: true
+        })
+        if(!bootcamp){
+            return res.status(400).json({success: false})
+        }
+        res.status(200).json({success: true, data: bootcamp})
+    }
+    catch(err){
+        return res.status(400).json({success: false})
+    }
+}
+
+// @desc -> delete single bootcamp
+// @route -> DELETE /api/bootcamps/:id
+// @access -> Public
+exports.deleteBootcamp = async (req, res, next) => {
+    try{
+        const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+        if(!bootcamp){
+            return res.status(400).json({success: false})
+        }
+        res.status(200).json({success: true, data: {} })
+    }
+    catch(err){
+        return res.status(400).json({success: false})
+    }
+}
+
 
  
--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const { getBootcamps, createBootcamp, getBootcamp } = require('../controllers/bootcamps')
+const { getBootcamps, createBootcamp, getBootcamp, updateBootcamp, deleteBootcamp } = require('../controllers/bootcamps')
 
 router.route('/').get(getBootcamps)
 router.route('/:id').get(getBootcamp)
+router.route('/:id').put(updateBootcamp)
+router.route('/:id').delete(deleteBootcamp)
 router.route('/create_bootcamp').post(createBootcamp)
 
 module.exports = router;
