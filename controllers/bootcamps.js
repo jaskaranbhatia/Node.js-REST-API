ddf6da371169f10b439b222a8f80ae381d3c7737
jaskaranbhatia <jaskaransingh.singh77@gmail.com>
2021-06-18 10:15:13 +0530

    Sorting and Selection Added

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -7,9 +7,41 @@ const ErrorResponse = require('../util/errorResponse')
 // @access -> Public
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
     let query;
-    let queryStr = JSON.stringify(req.query);
+
+    // Copy req.query
+    const reqQuery = { ...req.query };
+
+    // Fields to exclude for filtering
+    const removeFields = ['select', 'sort']
+
+    // Loop over removeFields and delete them from reqQuery
+    removeFields.forEach(param => delete reqQuery[param]);  
+
+    // Create query string
+    let queryStr = JSON.stringify(reqQuery);
+
+    // Create operators ($gt, $gte, etc)
     queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
-    const bootcamps = await Bootcamp.find(JSON.parse(queryStr));
+    
+    query = Bootcamp.find(JSON.parse(queryStr));
+
+    // Select Fields
+    if(req.query.select){
+        const fields = req.query.select.split(',').join(' ');
+        query = query.select(fields)
+    }
+
+    // Sort
+    if(req.query.sort){
+        const sortBy = req.query.sort.split(',').join(' ')
+        query = query.sort(sortBy);
+    }
+    else{
+        query = query.sort('-createdAt');
+    }
+
+    const bootcamps = await query;
+    
     res.status(200).json({
         success: true,
         count: bootcamps.length,
